feea44ab931206eb43e943e6a1c262cec6635fca
m1n337 <m1n337b@gmail.com>
2024-01-07 00:55:58 +0800

    add: husky & lint-staged

--- cli.js
+++ cli.js
@@ -11,7 +11,7 @@ program
   .version('0.0.1');
 
 program
-  .command('evm')
+    .command('evm')
   .description('Ethereum wallet managemen')
   .option('-g, --generate [number]', 'Generate ethereum account')
   .option('-m, --mnemonic', 'Enter mnemonic interactively')
